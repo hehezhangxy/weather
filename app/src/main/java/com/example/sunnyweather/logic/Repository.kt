6b4e3d28b3592e39a685df7a7b237c53059d6243
zhang <zhang@qq.com>
2020-09-22 10:46:48 +0800

    城市搜索

--- app/src/main/java/com/example/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -1,30 +1,46 @@
 package com.example.sunnyweather.logic
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import com.example.sunnyweather.logic.model.Place
 import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
+
 import kotlinx.coroutines.Dispatchers
-import retrofit2.http.Query
-import javax.xml.transform.Result
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlin.coroutines.CoroutineContext
 
-class Repository {
-    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
-        val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
-        if (placeResponse.status == "ok") {
-            val places = placeResponse.places
-            kotlin.Result.success(places)
-        } else {
-            kotlin.Result.failure(RuntimeException("response status is ${placeResponse.status}"))
-        }
-    }
-    private fun <T> fire(context: CoroutineContext, block: suspend () -> kotlin.Result<T>) =
-        liveData<kotlin.Result<T>>(context) {
-            val result = try {
-                block()
-            } catch (e: Exception) {
-                kotlin.Result.failure<T>(e)
+object Repository {
+    fun <T> searchPlaces(query: String) = liveData<kotlin.Result<T>>(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
             }
-            emit(result)
+        } catch (e: Exception) {
+            Result.failure<T>(e)
         }
+        emit(result as Result<T>)
+    }
+//    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
+//        val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+//        if (placeResponse.status == "ok") {
+//            val places = placeResponse.places
+//            Result.success(places)
+//        } else {
+//            Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+//        }
+//    }
+//    private fun <T> fire(context: CoroutineContext, block: suspend () -> kotlin.Result<T>) =
+//        liveData<kotlin.Result<T>>(context) {
+//            val result = try {
+//                block()
+//            } catch (e: Exception) {
+//                Result.failure<T>(e)
+//            }
+//            emit(result)
+//        }
+
 }
--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.sunnyweather.ui.place
 
-class PlaceViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.example.sunnyweather.logic.Repository
+import com.example.sunnyweather.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+    val placeLiveData = Transformations.switchMap(searchLiveData) {query ->
+        Repository.searchPlaces<List<Place>>(query)
+    }
+
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+
+//    fun savePlace(place: Place) = Repository.savePlace(place)
+//
+//    fun getSavedPlace() = Repository.getSavedPlace()
+//
+//    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
